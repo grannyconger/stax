6cde362c94c9fb063d454d62235a8dab898be145
Brian Weaver <bweaver@greermiddlecollege.org>
2026-03-10 21:30:12 -0400

    Update DATA_URL to new endpoint

--- config.js
+++ config.js
@@ -16,5 +16,5 @@
 // ============================================================
 
 const CONFIG = {
-  DATA_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=AY5xjrT2NTrb8nAxgrqzFTf9rrFlTCn1Ai9fUxcCzGfcys4ckBtNs98C2ad9dAHnF-HG7qVaZh009ooeVQkwnLznBxbvXEXR82tpJfwCvslC9Hqs3QEskooH4dSni7vkyLvCAyC9vbyzZULl8Xr74kbTuaXPJcv7M_wTw9hIm9C37TL1G7-iGobroHJ_b86AKxiLBtyMgROcE5qcezxTzPCptVH71FO33304uj7i3SiOsW2zf-KQLq7NyG-MCAjLFUZooBDfYvfVTZxYN5D1JsNHKttOullMPZ4R-hzExaJf&lib=MfIfTLftmFK2wFaeKKt9cDRbhdow-Xxju"
+  DATA_URL: "https://script.google.com/macros/s/AKfycbxlAACMKLlxD21XkBVFhm37kYaUhoAq0-cOSQlgKLMCBke0cJv_Wod4ldgCm6HSXy5q5w/exec"
 };
